7cb4f16b89815ab141069f34f06093121794cb15
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-06-14 04:31:01 +0800

    (adjust): Add 1 node in populate-local-nodes.sh

--- populate-local-nodes.sh
+++ populate-local-nodes.sh
@@ -3,24 +3,30 @@ set -e
 # image tag is `yugabytedb/yugabyte:2.21.1.0-b271-aarch64``
 export IMAGE_SHA=848ff36bf479eec0cfe6af99b995cc2bb0dab11b0cc2d931302288a346f4f787
 
+docker rm -f yugabytedb-node1 yugabytedb-node2 yugabytedb-node3
+
 docker network rm -f yugabytedb-network
 docker network create yugabytedb-network
 
 docker run -d --name yugabytedb-node1 --net yugabytedb-network \
-    -p 15433:15433 -p 5433:5433 \
+    -p :15433 -p :5433 \
     --restart unless-stopped \
     $IMAGE_SHA \
     bin/yugabyted start --background=false
 
-sleep 10
-
 docker run -d --name yugabytedb-node2 --net yugabytedb-network \
-    -p 15434:15433 -p 5434:5433 \
+    -p :15433 -p :5433 \
     --restart unless-stopped \
     $IMAGE_SHA \
     bin/yugabyted start --join=yugabytedb-node1 --background=false
 
-sleep 10
+docker run -d --name yugabytedb-node3 --net yugabytedb-network \
+    -p :15433 -p :5433 \
+    --restart unless-stopped \
+    $IMAGE_SHA \
+    bin/yugabyted start --join=yugabytedb-node1 --background=false
+
+sleep 20
 
 docker exec -it yugabytedb-node1 bin/ysqlsh -h yugabytedb-node1 \
-     -c 'select * from yb_servers()'
+    -c 'select host, port from yb_servers()' | cat
